3e57d3b56d407fc6b82a9c1ee7fddee0d01e68e0
OPatrolin <t.largeron@bdx.ecolescreatives.com>
2026-03-30 13:18:15 +0200

    UPDATE playermovement
    Player can now move in CharaControler

--- My project/Assets/Scripts/CharaControler.cs
+++ My project/Assets/Scripts/CharaControler.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class CharaControler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Speed variable")]
+    [SerializeField] float speed = 3f;
+    float inputX;
+    float inputY;
 
-    // Update is called once per frame
+    //Rigidbody du joueur.
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        
+        //Récupère la direction du joueur sur les deux axes en prennant en compte la sensibilité pour joysticks.
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Attribue à la vélocité du rigidboy le sens de déplacement du joueur * la vitesse.
+        var v = rb.linearVelocity;
+        v.x = inputX * speed;
+        v.y = inputY * speed;
+        rb.linearVelocity = v;
     }
 }
