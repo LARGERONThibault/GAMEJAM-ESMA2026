82cd28a9c69d221675a39bf127abbd71602d9f12
OPatrolin <t.largeron@bdx.ecolescreatives.com>
2026-03-30 19:00:47 +0200

    UPDATE enemy AI
    Implemented better patrol and the entire chase behavior.

--- My project/Assets/Scripts/Shadows/ShadowsManager.cs
+++ My project/Assets/Scripts/Shadows/ShadowsManager.cs
@@ -10,10 +10,19 @@ public class ShadowsManager : MonoBehaviour
     //0 = patrouille, 1 = pourchasse le joueur;
     bool state;
 
-    //Variables liées au temps.
+    [Header("Déplacement.")]
     [SerializeField] float walkspeed = 5f;
     [SerializeField] float waittime = 3f;
-    
+
+    [Header("Attaque.")]
+    [SerializeField] float speedmult = 5f;
+    [SerializeField] float chasetime = 2f;
+
+    [Header("Detection.")]
+    [SerializeField] float viewrange = 3f;
+    [SerializeField] Transform playertransform;
+    [SerializeField] LayerMask enviroMask;
+    [SerializeField] LayerMask playerMask;
 
     //Liste des coordonées où il doit se rendre.
     public List<Transform> coordinates;
@@ -23,11 +32,24 @@ public class ShadowsManager : MonoBehaviour
 
     void Start()
     {
+        
         //Spawn à la première coordonnée et ira à la seconde.
         transform.position = coordinates[0].position;
         next = coordinates[currentcoo];
 
+        PatrolMode();
+    }
+
+    void PatrolMode()
+    {
         StartCoroutine(Patrol(next));
+        StartCoroutine(Detection());
+    }
+
+    void ChaseMode()
+    {
+         StopAllCoroutines();
+        StartCoroutine(Chase());
     }
     
     
@@ -57,17 +79,62 @@ public class ShadowsManager : MonoBehaviour
             currentcoo += 2 * walkdirection;
         }
 
-        Debug.Log(currentcoo);
         next = coordinates[currentcoo];
 
         StartCoroutine(Patrol(next));
     }
 
+    void Observe(Vector2 direction)
+    {
+        RaycastHit2D detect = Physics2D.Raycast(transform.position, transform.TransformDirection(direction), viewrange, playerMask);
+        Debug.DrawRay(transform.position, transform.TransformDirection(direction)*viewrange,Color.red,1f);
+        {
+            if (detect)
+            {
+                {
+                    ChaseMode();
+                    Debug.Log("Vu");
+                }
+            }
+            
+        }
+    }
+
+    IEnumerator Chase()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playertransform.position, walkspeed * speedmult * Time.deltaTime);
+            yield return new WaitForSecondsRealtime(0.01f);
+        }
+    }
+
+    IEnumerator Detection()
+    {
+        Observe(Vector2.down);
+        Observe(new Vector2(0.5f,-0.5f));
+        Observe(Vector2.right);
+        Observe(new Vector2(0.5f, 0.5f));
+        Observe(Vector2.up);
+        Observe(new Vector2(-0.5f, 0.5f));
+        Observe(Vector2.left);
+        Observe(new Vector2(-0.5f, -0.5f));
+        for (int i = 0; i < 40; i++)
+        {
+            yield return null;
+        }
+        StartCoroutine(Detection());
+    }
+
 
     //Gère la mort au contact
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collided)
     {
-        SceneManager.LoadScene("DeathScene");
+            if (collided.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("DeathScene");
+        }
     }
+
 
 }
